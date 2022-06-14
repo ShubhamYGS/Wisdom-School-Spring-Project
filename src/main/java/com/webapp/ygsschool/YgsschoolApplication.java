2533d4d3cb5afa299907edf57361fd49ea892405
ShubhamYGS <s.patil14569@gmail.com>
2022-06-14 20:42:59 +0530

    Initial commit

---
+++ src/main/java/com/webapp/ygsschool/YgsschoolApplication.java
@@ -0,0 +1,13 @@
+package com.webapp.ygsschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YgsschoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YgsschoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webapp/ygsschool/YgsschoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.ygsschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YgsschoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
