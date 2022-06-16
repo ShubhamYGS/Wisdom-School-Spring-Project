95163118702cdd1d598f8d0a48c1d369c0fc2d15
ShubhamYGS <s.patil14569@gmail.com>
2022-06-16 19:26:10 +0530

    Added thymeleaf template engine to project

--- src/main/java/com/webapp/ygsschool/controller/HomeController.java
+++ src/main/java/com/webapp/ygsschool/controller/HomeController.java
@@ -1,13 +1,15 @@
 package com.webapp.ygsschool.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping("/home")
-    public String showHomePage(){
+    @RequestMapping(value= {"","/","/home"})
+    public String showHomePage(Model model){
+        model.addAttribute("username","Shubham");
         return "home.html";
     }
 }
