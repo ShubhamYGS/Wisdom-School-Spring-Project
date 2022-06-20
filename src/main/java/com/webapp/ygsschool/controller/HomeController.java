b9ee7f2a2568a190025fbf8bf899588a7bdcb8c6
ShubhamYGS <s.patil14569@gmail.com>
2022-06-20 09:56:05 +0530

    Separated Header-Footer & added WebMvcConfigurer

--- src/main/java/com/webapp/ygsschool/controller/HomeController.java
+++ src/main/java/com/webapp/ygsschool/controller/HomeController.java
@@ -7,13 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-    @RequestMapping(value= {"","/","/index.html"})
+    @RequestMapping(value= {"","/","index"})
     public String showHomePage(){
         return "index.html";
     }
 
-    @RequestMapping("/testing.html")
-    public String showTestingPage() {
-        return "testing.html";
-    }
 }
